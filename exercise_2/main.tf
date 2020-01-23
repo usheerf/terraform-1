5d680bb9743c5c9872120b898e46966eddb9ee1d
usheerf <56785704+usheerf@users.noreply.github.com>
2020-01-23 12:30:05 -0800

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "usheer-tf-remote-state-bucket"
    key = "terraform.tfstate"
    region = "us-east-1"
   }
